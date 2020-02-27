2f0dc9176b057f70fa0d3006ff2b5c56d39f394a
Neil Flatley <n.flatley@zengenti.com>
2020-02-27 23:05:23 +0000

    refactor/fix: replace xxhash package that requires python to just install it from cpp source with another that has been proven to provide the exact same output but without the developer / deployment overhead

--- src/server/util/cacheHashing.js
+++ src/server/util/cacheHashing.js
@@ -1,10 +1,20 @@
 export const hashKeys = keys => {
-  const XXHash = require('xxhash');
+  // const XXHash = require('xxhash');
+  const XXHash = require('xxhashjs');
   const returnKeys = [];
   keys.forEach(cacheKey => {
     const inputBuffer = Buffer.from(cacheKey.toLowerCase(), 'utf-8');
-    const outputBuffer = Buffer.alloc(4);
-    XXHash.hash(inputBuffer, 0x0, outputBuffer);
+    // const outputBuffer = Buffer.alloc(4);
+    // XXHash.hash(inputBuffer, 0x0, outputBuffer);
+
+    const hashed = XXHash.h32(inputBuffer, 0x0).toString(16);
+    const reversedhex = hashed
+      .match(/[a-fA-F0-9]{2}/g)
+      .reverse()
+      .join('');
+
+    const outputBuffer = Buffer.from(reversedhex, 'hex');
+
     returnKeys.push(outputBuffer.toString('base64').substring(0, 6));
   });
   return returnKeys;
