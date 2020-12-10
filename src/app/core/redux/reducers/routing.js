24fd300c5004a00120a1e044fd1f65f836fbec45
Neil Flatley <n.flatley@zengenti.com>
2020-12-10 10:50:25 +0000

    fix: issue with mappedEntry not being written to the redux state in v2 only due to iffy Object.keys check

--- src/app/core/redux/reducers/routing.js
+++ src/app/core/redux/reducers/routing.js
@@ -59,7 +59,7 @@ export default (state = initialState, action) => {
           .set('isLoading', isLoading)
           .set('notFound', notFound);
 
-        if (mappedEntry && Object.keys(mappedEntry) > 0)
+        if (mappedEntry && Object.keys(mappedEntry).length > 0)
           nextState = nextState
             .set('mappedEntry', fromJS(mappedEntry))
             .set('entry', fromJS({ sys: entry.sys }));
