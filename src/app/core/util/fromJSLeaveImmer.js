3b193fb2bd29c62743ab54b9746964da978c6820
Mark Turner <m.turner@zengenti.com>
2021-02-10 14:11:15 +0000

    fix: issue with SSR not hydrating properly/flash alternative way of handling immer features

--- src/app/core/util/fromJSLeaveImmer.js
+++ src/app/core/util/fromJSLeaveImmer.js
@@ -1,21 +1,24 @@
-import { Map, OrderedMap, fromJS } from 'immutable';
 import fromJSOrdered from './fromJSOrdered';
 
-const fromJSLeaveImmer = (js, isOrdered = false) => {
-  console.info(js);
-  if (typeof js !== 'object' || js === null) return js;
-  // console.info(`from js - here is js ${JSON.stringify(js)}`);
-  const convertedObject = isOrdered ? OrderedMap() : Map();
-  const keys = Object.keys(js);
-  keys.forEach(key => {
-    if (key === 'immer') {
-      convertedObject.set(key, js[key]);
-      // console.info(`LOOK! - immer untouched bar root key "${key}"`);
-    } else {
-      // console.info(`LOOK! - normal immutable feature "${key}"`);
-      convertedObject.set(key, isOrdered ? fromJSOrdered(js) : fromJS(js));
-    }
-  });
+const fromJSLeaveImmer = js => {
+  // console.info(js);
+  // if (typeof js !== 'object' || js === null) return js;
+  // // console.info(`from js - here is js ${JSON.stringify(js)}`);
+  // const convertedObject = isOrdered ? OrderedMap() : fromJS({});
+  // const keys = Object.keys(js);
+  // keys.forEach(key => {
+  //   if (key === 'immer') {
+  //     convertedObject.set(key, js[key]);
+  //     // console.info(`LOOK! - immer untouched bar root key "${key}"`);
+  //   } else {
+  //     // console.info(`LOOK! - normal immutable feature "${key}"`);
+  //     convertedObject.set(key, isOrdered ? fromJSOrdered(js) : fromJS(js));
+  //   }
+  // });
+  const immutableObj = fromJSOrdered(js);
+  if (immutableObj && !!immutableObj.get('immer')) {
+    immutableObj.set('immer', immutableObj.get('immer').toJS());
+  }
+  return immutableObj;
 };
-
 export default fromJSLeaveImmer;
--- src/server/core/webApp.js
+++ src/server/core/webApp.js
@@ -9,8 +9,8 @@ import { ServerStyleSheet } from 'styled-components';
 import Helmet from 'react-helmet';
 import serialize from 'serialize-javascript';
 import minifyCssString from 'minify-css-string';
-// import { fromJS } from 'immutable';
-import fromJSLeaveImmer from '~/core/util/fromJSLeaveImmer';
+import { fromJS } from 'immutable';
+// import fromJSLeaveImmer from '~/core/util/fromJSLeaveImmer';
 import fromEntries from 'fromentries';
 
 import { history } from '~/core/redux/history';
@@ -182,7 +182,7 @@ const webApp = (app, ReactApp, config) => {
     // Create a store (with a memory history) from our current url
     const store = createStore(
       withReducers,
-      fromJSLeaveImmer({}),
+      fromJS({}),
       history({
         initialEntries: [url],
       })
