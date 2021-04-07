364f84798c532d56cbba4d60f267ca21fade8ceb
d.petrovic <d.petrovic@zengenti.com>
2021-04-07 09:54:36 +0100

    Implement useUser hook to provide a "master" container for all registration/user services

--- src/app/features/login/containers/useUser.js
+++ src/app/features/login/containers/useUser.js
@@ -1,16 +1,65 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCurrentSearch } from '~/core/redux/selectors/routing';
 import {
-  useLogin,
-  useRegistration,
-  useForgotPassword,
-  useResetPassword,
-} from '-/src/lib/user';
+  loginUser,
+  logoutUser,
+  registerUser,
+  requestPasswordReset,
+  resetPassword,
+} from '../redux/actions';
+import {
+  selectUser,
+  selectUserAuthenticationError,
+  selectUserError,
+  selectUserIsAuthenticated,
+  selectUserIsLoading,
+  selectUserRegistrationError,
+  selectUserRegistrationIsLoading,
+  selectUserRegistrationIsSuccess,
+  selectUserRegistration,
+  selectPasswordResetRequestSending,
+  selectPasswordResetRequestSent,
+  selectPasswordResetRequestError,
+  selectResetPasswordSending,
+  selectResetPasswordSent,
+  selectResetPasswordError,
+  selectUserAuthenticationErrorMessage,
+  selectUserErrorMessage,
+} from '../redux/selectors';
 
 const useUser = () => {
+  const dispatch = useDispatch();
+  const select = useSelector;
+
   return {
-    ...useLogin(),
-    ...useRegistration(),
-    ...useForgotPassword(),
-    ...useResetPassword(),
+    loginUser: (username, password) => dispatch(loginUser(username, password)),
+    logoutUser: redirectPath => dispatch(logoutUser(redirectPath)),
+    authenticationError: select(selectUserAuthenticationError),
+    authenticationErrorMessage: select(selectUserAuthenticationErrorMessage),
+    userError: select(selectUserError),
+    userErrorMessage: select(selectUserErrorMessage),
+    isAuthenticated: select(selectUserIsAuthenticated),
+    userIsLoading: select(selectUserIsLoading),
+    user: select(selectUser).toJS(),
+
+    registerUser: (user, mappers) => dispatch(registerUser(user, mappers)),
+    registrationError: select(selectUserRegistrationError),
+    registrationIsLoading: select(selectUserRegistrationIsLoading),
+    registrationIsSuccess: select(selectUserRegistrationIsSuccess),
+    userRegistration: select(selectUserRegistration).toJS(),
+
+    passwordResetRequestIsLoading: select(selectPasswordResetRequestSending),
+    passwordResetRequestIsSuccess: select(selectPasswordResetRequestSent),
+    passwordResetRequestError: select(selectPasswordResetRequestError),
+    requestPasswordReset: userEmailObject =>
+      dispatch(requestPasswordReset(userEmailObject)),
+
+    queryString: select(selectCurrentSearch),
+    passwordResetIsLoading: select(selectResetPasswordSending),
+    passwordResetIsSuccess: select(selectResetPasswordSent),
+    passwordResetError: select(selectResetPasswordError),
+    resetPassword: resetPasswordObject =>
+      dispatch(resetPassword(resetPasswordObject)),
   };
 };
 
