2c0d3447cbaa7cb9164bcdfcd1be6df6894b8c15
Neil Flatley <n.flatley@zengenti.com>
2021-09-05 01:53:36 +0100

    build: add types to AppConfig keys

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,14 @@
+import { ContentTypeMapping, StaticRoute, WithEvents } from './routing/routes';
 import handleResponse from './server/features/response-handler';
 
 export type AppConfig = {
-  routes: any;
-  withReducers: any;
-  withSagas: any;
-  withEvents: any;
+  routes: {
+    ContentTypeMappings: ContentTypeMapping[];
+    StaticRoutes: StaticRoute[];
+  };
+  withReducers: { [key: string]: any };
+  withSagas: any[];
+  withEvents: WithEvents;
 };
 
 export type ServerConfig = AppConfig & {
