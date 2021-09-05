3af269526927a0f0b4a9a79456f0855771b8b803
nflatley-zengenti <github@zengenti.com>
2021-09-05 00:40:32 +0000

    ci: bumps version [skip ci]

--- cjs/search.js
+++ cjs/search.js
@@ -23,7 +23,9 @@ const toJS = WrappedComponent => wrappedComponentProps => {
   const KEY = 0;
   const VALUE = 1;
   const propsJS = Object.entries(wrappedComponentProps).reduce((newProps, wrappedComponentProp) => {
-    newProps[wrappedComponentProp[KEY]] = 'toJS' in wrappedComponentProp[VALUE] ? wrappedComponentProp[VALUE].toJS() : wrappedComponentProp[VALUE];
+    const propKey = wrappedComponentProp[KEY];
+    const propValue = wrappedComponentProp[VALUE];
+    newProps[propKey] = typeof propValue === 'object' && 'toJS' in propValue ? propValue.toJS() : propValue;
     return newProps;
   }, {});
   return /*#__PURE__*/React__default['default'].createElement(WrappedComponent, propsJS);
@@ -1125,7 +1127,7 @@ const searchUriTemplate = {
     const searchContext = getSearchContext(state); // Lose stateFilters and currentSearch if a new
     // term is passed via an argument
 
-    const stateFilters = term ? immutable.List() : getSelectedFilters(state, facet, searchContext).map(f => f.join(','));
+    const stateFilters = term ? immutable.List() : getSelectedFilters(state, facet, searchContext).map(f => f === null || f === void 0 ? void 0 : f.join(','));
     const currentSearch = !term && state.getIn(['routing', 'location', 'search']);
     const currentQs = removeEmptyAttributes(queryString__default['default'].parse(currentSearch));
     if (orderBy) currentQs.orderBy = orderBy;
@@ -1389,7 +1391,7 @@ const queryParamsTemplate = {
     state,
     facet,
     context
-  }) => getSelectedFilters(state, facet, context).map(f => f.join(',')),
+  }) => getSelectedFilters(state, facet, context).map(f => f === null || f === void 0 ? void 0 : f.join(',')),
   versionStatus: ({
     state
   }) => selectVersionStatus(state),
