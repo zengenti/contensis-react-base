6fd2c9ab5c555f6f1524365677edf6ebe35e3563
Neil Flatley <n.flatley@zengenti.com>
2021-10-05 18:26:45 +0100

    fix: add form to fromJSLeaveImmer as this is now converted to use immer reducers

--- src/util/fromJSLeaveImmer.js
+++ src/util/fromJSLeaveImmer.js
@@ -1,20 +1,6 @@
 import fromJSOrdered from './fromJSOrdered';
 
 const fromJSLeaveImmer = js => {
-  // console.info(js);
-  // if (typeof js !== 'object' || js === null) return js;
-  // // console.info(`from js - here is js ${JSON.stringify(js)}`);
-  // const convertedObject = isOrdered ? OrderedMap() : fromJS({});
-  // const keys = Object.keys(js);
-  // keys.forEach(key => {
-  //   if (key === 'immer') {
-  //     convertedObject.set(key, js[key]);
-  //     // console.info(`LOOK! - immer untouched bar root key "${key}"`);
-  //   } else {
-  //     // console.info(`LOOK! - normal immutable feature "${key}"`);
-  //     convertedObject.set(key, isOrdered ? fromJSOrdered(js) : fromJS(js));
-  //   }
-  // });
   const immutableObj = fromJSOrdered(js);
   if (
     immutableObj &&
@@ -24,10 +10,12 @@ const fromJSLeaveImmer = js => {
     // convert the immer parts of the state back
     // to plain JS while retuning an immutable state object
     let immutableState = immutableObj;
-    ['immer', 'navigation', 'routing', 'search', 'user', 'version'].map(key => {
-      if (js[key] && immutableObj.get(key))
-        immutableState = immutableState.set(key, js[key]);
-    });
+    ['immer', 'form', 'navigation', 'routing', 'search', 'user', 'version'].map(
+      key => {
+        if (js[key] && immutableObj.get(key))
+          immutableState = immutableState.set(key, js[key]);
+      }
+    );
     return immutableState;
   }
   return immutableObj;
