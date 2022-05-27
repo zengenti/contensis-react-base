2ccc861839021b7c4a9a897f95261bb23df32354
nflatley-zengenti <github@zengenti.com>
2022-05-27 18:18:43 +0000

    ci: bumps version [skip ci]

--- models/forms/index.d.ts
+++ models/forms/index.d.ts
@@ -1,3 +1,3 @@
-export * from 'zengenti-forms-package';
-import Form from 'zengenti-forms-package';
+export * from 'zengenti-forms-package/src/app/zengenti-forms-package';
+import Form from 'zengenti-forms-package/src/app/zengenti-forms-package';
 export default Form;
