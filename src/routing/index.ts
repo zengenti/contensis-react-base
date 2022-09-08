a36e2a539cae7b5b70ad254bb65703d14d159c27
Derry Coffey <d.coffey@zengenti.com>
2022-09-08 09:21:35 +0100

    chore: Add export for Redirect and Status from routing index.

--- src/routing/index.ts
+++ src/routing/index.ts
@@ -3,4 +3,6 @@ export * as types from './redux/types';
 export * as actions from './redux/actions';
 export * as selectors from './redux/selectors';
 export { useHttpContext } from './httpContext';
+export { Redirect } from './components/Redirect';
+export { Status } from './components/Status';
 export { default as RouteLoader } from './components/RouteLoader';
