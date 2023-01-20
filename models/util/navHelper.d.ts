b74a526b9f2dc3506dce3a1a5fec608dbf713c9a
Neil Flatley <n.flatley@zengenti.com>
2023-01-20 14:21:59 +0000

    fix: search package null errors when replacing entire search state with a fresh config, remove redundant nav code

--- models/util/navHelper.d.ts
+++
@@ -1,7 +0,0 @@
-export function componentWillMountNavHandler(props: any, urlPropsQueryConfig: any): void;
-export function componentWillReceivePropsNavHandler(nextProps: any, component: any, urlPropsQueryConfig: any, currentPath: any): void;
-export function getUrlFromNavigationSettings(entry: any): string;
-export function getParentUrlFromNavigationSettings(entry: any): string;
-export function getLeafSlugFromRoute(currentPath: any): any;
-export function validateRouteFromNavigationSettings(currentPath: any, entry: any): boolean;
-export function getPathFromEntry(entry: any): any;
