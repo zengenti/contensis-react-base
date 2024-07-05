8f0840f010527c8500c46b52ceedb7896601f427
Neil Flatley <n.flatley@zengenti.com>
2024-07-05 17:46:40 +0100

    fix: TypeScript error in StaticRoutes files when used with Loadable components

--- src/models/RouteComponent.ts
+++ src/models/RouteComponent.ts
@@ -1,1 +1,6 @@
-export type RouteComponent<Props> = React.ComponentType<Props>;
+import { LoadableComponent } from '@loadable/component';
+import { RouteComponentProps } from './RouteComponentProps';
+
+export type RouteComponent<Props> =
+  | LoadableComponent<RouteComponentProps>
+  | React.ComponentType<Props>;
