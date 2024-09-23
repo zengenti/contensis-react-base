dc2593d095b70831afd4631b8399e31b1a81455e
tbreeze-zengenti <github@zengenti.com>
2024-09-23 12:36:03 +0000

    ci: bumps version to 3.2.2-beta.9 [skip ci]

--- models/models/ContentTypeMapping.d.ts
+++ models/models/ContentTypeMapping.d.ts
@@ -5,25 +5,85 @@ import { RouteComponent } from './RouteComponent';
 import { RouteComponentProps } from './RouteComponentProps';
 import { RequireLogin } from './RequireLogin';
 export type ContentTypeMapping = {
+    /** The unique identifier for the content type to map. */
     contentTypeID: string;
+    /**
+     * The React component that should be rendered for this content type.
+     */
     component: RouteComponent<RouteComponentProps>;
+    /**
+     * Optional entry mapper to transform entry data before passing it to the component.
+     */
     entryMapper?: EntryMapper;
+    /**
+     * An array of field IDs used to restrict the fields returned for an entry.
+     */
     fields?: string[];
+    /**
+     * Injects Redux dependencies or actions into the route if needed.
+     */
     injectRedux?: ReduxInjector;
+    /**
+     * The depth at which to resolve the full entry data for a linked entry or asset.
+     * Maximum depth is 10, with a recommended maximum of 4.
+     *
+     * A default depth of 2 is applied via `routeLoadOptions` in `onRouteLoad`.
+     */
     linkDepth?: number;
+    /**
+     * Custom link depths for specific field paths to resolve full entry data for a linked entry or asset.
+     * Maximum depth is 10, with a recommended maximum of 4.
+     *
+     * This option is available only in Contensis version 16+.
+     */
     fieldLinkDepths?: FieldLinkDepths;
+    /**
+     * Options for configuring how Site View Node data is handled, including children and siblings.
+     */
     nodeOptions?: {
         children?: {
+            /** The depth of descendants to include for the node. */
             depth: number;
+            /**
+             * An array of field IDs used to restrict the fields returned for each entry.
+             */
             fields?: string[];
+            /**
+             * The depth at which to resolve the full entry data for a linked entry or asset.
+             * Maximum depth is 10, with a recommended maximum of 4.
+             *
+             * A default depth of 2 is applied via `routeLoadOptions` in `onRouteLoad`.
+             */
             linkDepth?: number;
+            /**
+             * Custom link depths for specific field paths to resolve full entry data for a linked entry or asset.
+             * Maximum depth is 10, with a recommended maximum of 4.
+             *
+             * This option is available only in Contensis version 16+.
+             */
             fieldLinkDepths?: FieldLinkDepths;
         } | boolean;
         siblings?: {
+            /** The depth of descendants to include for the node. */
             fields?: string[];
+            /**
+             * The depth at which to resolve the full entry data for a linked entry or asset.
+             * Maximum depth is 10, with a recommended maximum of 4.
+             *
+             * A default depth of 2 is applied via `routeLoadOptions` in `onRouteLoad`.
+             */
             linkDepth?: number;
+            /**
+             * Custom link depths for specific field paths to resolve full entry data for a linked entry or asset.
+             * Maximum depth is 10, with a recommended maximum of 4.
+             *
+             * This option is available only in Contensis version 16+.
+             */
             fieldLinkDepths?: FieldLinkDepths;
         } | boolean;
     };
+    /**
+     * Specifies whether login is required to access the content for this type.
+     */
     requireLogin?: RequireLogin;
 };
--- models/models/WithEvents.d.ts
+++ models/models/WithEvents.d.ts
@@ -6,7 +6,15 @@ export type OnRouteLoadArgs = Omit<GetRouteActionArgs, 'withEvents'>;
 export type OnRouteLoadedArgs = OnRouteLoadArgs & {
     entry?: Entry | any;
 };
+/**
+ * Configuration options to customize the app state during route changes.
+ * These options are evaluated on each route action, establishing the "default" behavior.
+ */
 export type RouteLoadOptions = {
+    /**
+     * Determines whether to query related Site View Nodes by default during routing.
+     * Allows specifying the depth of query for ancestors, children, siblings, and the entire tree structure.
+     */
     customNavigation?: boolean | {
         ancestors: boolean | number;
         children: boolean | number;
@@ -14,9 +22,30 @@ export type RouteLoadOptions = {
         tree: boolean | number;
     };
     customRouting?: boolean;
+    /**
+     * Sets the default language for the Site View during route changes.
+     *
+     * @default 'en-GB'
+     */
     defaultLang?: string;
+    /**
+     * Defines the default depth for resolving full entry data for a linked entry or asset during route changes.
+     * Maximum depth is 10, with a recommended maximum of 4.
+     *
+     * @default 2
+     */
     entryLinkDepth?: number;
+    /**
+     * Specifies custom depths for resolving full entry data for specific field paths in linked entries or assets during route changes.
+     * Maximum depth is 10, with a recommended maximum of 4.
+  
+     *
+     * Available only in Contensis version 16+.
+     */
     entryFieldLinkDepths?: FieldLinkDepths;
+    /**
+     * Disables the automatic scroll-to-top behavior when navigating to a new route.
+     */
     preventScrollTop?: boolean;
     refetchNode?: true;
 };
