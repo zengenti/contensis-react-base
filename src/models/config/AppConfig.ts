7687d0205b3f90a2971ba0a4f03144132d97aee2
Neil Flatley <n.flatley@zengenti.com>
2025-07-30 12:43:15 +0100

    refactor: mark `stateType` in `AppConfig` as deprecated due to immutable feature deprecation

--- src/models/config/AppConfig.ts
+++ src/models/config/AppConfig.ts
@@ -3,6 +3,7 @@ import { WithEvents } from '../WithEvents';
 import { StateType } from './StateType';
 
 export type AppConfig = {
+  /** @deprecated Immutable.js library is deprecated and may be removed in future versions. A plain js (and immutable) state is provided by immer package by default */
   stateType?: StateType;
   routes: AppRoutes;
   withReducers: { [key: string]: any };
