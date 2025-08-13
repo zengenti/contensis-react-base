fe8046237a6fe69e055918539b42e8753e80783e
Neil Flatley <n.flatley@zengenti.com>
2025-08-13 18:33:21 +0100

    chore: redundant file

--- config/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rootDir: '../',
-  testPathIgnorePatterns: [
-    '<rootDir>/dist/',
-    '<rootDir>/node_modules/',
-    '<rootDir>/src/app/core',
-    '<rootDir>/src/server',
-  ],
-};
