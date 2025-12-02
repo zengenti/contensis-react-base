46bc00f36c910caba81ea0cba0c28a958548f4ed
Neil Flatley <n.flatley@zengenti.com>
2025-12-02 18:52:55 +0000

    feat: new `useI18n` hook to easily access internationalization features in components, providing a wrapped function to `updateLanguage` and a `translate` function to render dictionary phrases with

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,6 @@
 export * from './redux/selectors';
 export * from './redux/slice';
+export { useI18n } from './useI18n.hook';
+
 export type * from '../models/Locales';
 export type * from '../models/config/I18n';
