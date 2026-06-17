8797565e3b759b232c1782fcc17a709b5dc03c17
Neil Flatley <n.flatley@zengenti.com>
2026-06-17 17:37:30 +0100

    fix: reduce log noise when a `version.json` file is not available

--- src/server/util/getVersionInfo.ts
+++ src/server/util/getVersionInfo.ts
@@ -1,5 +1,13 @@
 import fs from 'fs';
 
+/**
+ * Arguably deprecated functionality driven by a version.json file
+ * injected into the container from the CI build process.
+ * This is used to provide version information for the application
+ * such as the commit id and the build number.
+ * @param staticFolderPath if the app is not built into the `dist/static` folder, we can provide a different path to the static folder.
+ * @returns version.json as a JSON object, or an empty object if the file cannot be read.
+ */
 export const getVersionInfo = (staticFolderPath: string) => {
   try {
     const versionData = fs.readFileSync(
@@ -9,7 +17,10 @@ export const getVersionInfo = (staticFolderPath: string) => {
     const versionInfo = JSON.parse(versionData);
     return versionInfo;
   } catch (ex) {
-    console.error(`Unable to read from "version.json"`, ex);
+    // We don't need to log this error as it is just noise in the logs
+    // when the version.json file is not constructed and injected into 
+    // the build container or a development stub included in the project.
+    // console.error(`Unable to read from "version.json"`, ex);
     return {};
   }
 };
