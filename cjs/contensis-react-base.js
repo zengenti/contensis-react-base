9cacef93dabdada7fca34fd7981816efb1f33ecd
nflatley-zengenti <github@zengenti.com>
2026-06-17 18:35:42 +0000

    ci: bumps version to 4.0.1-beta.11 [skip ci]

--- cjs/contensis-react-base.js
+++ cjs/contensis-react-base.js
@@ -1107,13 +1107,24 @@ const getBundleTags = (loadableExtractor, scripts, staticRoutePath = 'static') =
   return startupTag;
 };
 
+/**
+ * Arguably deprecated functionality driven by a version.json file
+ * injected into the container from the CI build process.
+ * This is used to provide version information for the application
+ * such as the commit id and the build number.
+ * @param staticFolderPath if the app is not built into the `dist/static` folder, we can provide a different path to the static folder.
+ * @returns version.json as a JSON object, or an empty object if the file cannot be read.
+ */
 const getVersionInfo = staticFolderPath => {
   try {
     const versionData = fs__default.default.readFileSync(`dist/${staticFolderPath}/version.json`, 'utf8');
     const versionInfo = JSON.parse(versionData);
     return versionInfo;
   } catch (ex) {
-    console.error(`Unable to read from "version.json"`, ex);
+    // We don't need to log this error as it is just noise in the logs
+    // when the version.json file is not constructed and injected into 
+    // the build container or a development stub included in the project.
+    // console.error(`Unable to read from "version.json"`, ex);
     return {};
   }
 };
--- models/server/util/getVersionInfo.d.ts
+++ models/server/util/getVersionInfo.d.ts
@@ -1,1 +1,9 @@
+/**
+ * Arguably deprecated functionality driven by a version.json file
+ * injected into the container from the CI build process.
+ * This is used to provide version information for the application
+ * such as the commit id and the build number.
+ * @param staticFolderPath if the app is not built into the `dist/static` folder, we can provide a different path to the static folder.
+ * @returns version.json as a JSON object, or an empty object if the file cannot be read.
+ */
 export declare const getVersionInfo: (staticFolderPath: string) => any;
