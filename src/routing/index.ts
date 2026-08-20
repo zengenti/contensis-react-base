341e273507d532109c11e46279cfadddeac0bffa
Neil Flatley <n.flatley@zengenti.com>
2026-08-20 16:34:46 +0100

    feat: export `NotFound` component in routing

--- src/routing/index.ts
+++ src/routing/index.ts
@@ -3,6 +3,7 @@ export * as types from './redux/types';
 export * as actions from './redux/actions';
 export * as selectors from './redux/selectors';
 export { useHttpContext } from './httpContext';
+export { default as NotFound } from './components/NotFound';
 export { Redirect } from './components/Redirect';
 export { Status } from './components/Status';
 export { default as RouteLoader } from './components/RouteLoader';
